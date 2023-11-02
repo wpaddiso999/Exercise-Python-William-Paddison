962c0350bc94cc0c9822879936f7ff7b0415b059
William Paddison <138761963+wpaddiso999@users.noreply.github.com>
2023-11-01 22:24:37 -0400

    Update movies.py

--- movies.py
+++ movies.py
@@ -1,33 +1,30 @@
 class Movies:
-    def __init__(self, movies_file):
-        self._movies = []
+    def __init__(self):
+        self._movies = []  # Initialize the _movies list
 
-        with open(movies_file, encoding="utf-8") as file:
-            row_idx = 0
+        # Load movie data from a text file and populate the _movies list
+        with open('movies.txt', 'r') as file:
             for line in file:
-                if row_idx%3 == 0:
-                    movie_name = line.rstrip()
-                if row_idx%3 == 1:
-                    movie_cast = line.rstrip().split(',')
-                if row_idx%3 == 2:
-                    self._movies.append(
-                        {
-                            'name': movie_name,
-                            'cast': movie_cast
-                        }
-                    )
-                    movie_name = None
-                    movie_cast = None
-                row_idx += 1
+                movie_data = line.strip().split(',')
+                if len(movie_data) == 2:
+                    movie_name, cast = movie_data
+                    cast_list = cast.split('|')
+                    movie = {'name': movie_name, 'cast': cast_list}
+                    self._movies.append(movie)
 
-        if movie_name and movie_cast:
-            # Add the last movie to the list
-            self._movies.append(
-                {
-                    'name': movie_name,
-                    'cast': movie_cast
-                }
-            )
+    def list_all_movie_names(self):
+        return [movie['name'] for movie in self._movies]
 
-if __name__ == "__main__":
-    movies = Movies('./movies.txt')
+    def search_movies_by_name(self, keyword):
+        keyword = keyword.lower()
+        matching_movies = [movie for movie in self._movies if keyword in movie['name'].lower()]
+        return [movie['name'] for movie in matching_movies]
+
+    def search_movies_by_cast(self, keyword):
+        keyword = keyword.lower()
+        matching_movies = []
+        for movie in self._movies:
+            matching_actors = [actor for actor in movie['cast'] if keyword in actor.lower()]
+            if matching_actors:
+                matching_movies.append((movie['name'], matching_actors))
+        return matching_movies
